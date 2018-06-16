bec11d76a247003caeff989d9bb21a7a3935b204
Karl Stenerud <kstenerud@gmail.com>
2018-06-16 06:57:38 -0700

    Tabs -> Spaces

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 QJSON
 =====
 
-Quick JSON parser and encoder in pure C.
+Quick JSON codec in pure C.
 
 
 
@@ -18,194 +18,213 @@ Features
 
 
 
+Building
+--------
+
+    mkdir build
+    cd build
+    cmake ..
+    make
+
+Produces: build/libqjson.a
+
+
+
+Testing
+-------
+
+    ./test/qjson_test
+
+
+
 Usage
 -----
 
 ### Encoding
 
-	#include <qjson/qjson.h>
-	#include <stdio.h>
+    #include <qjson/qjson.h>
+    #include <stdio.h>
 
-	void encode_example()
-	{
-	    int indent_spaces = 4;
-	    uint8_t buffer[1000];
-	    qjson_encode_context context = qjson_new_encode_context_with_config(buffer,
-	                                                                        buffer + sizeof(buffer),
-	                                                                        indent_spaces,
-	                                                                        DEFAULT_FLOAT_DIGITS_PRECISION);
+    void encode_example()
+    {
+        int indent_spaces = 4;
+        uint8_t buffer[1000];
+        qjson_encode_context context = qjson_new_encode_context_with_config(buffer,
+                                                                            buffer + sizeof(buffer),
+                                                                            indent_spaces,
+                                                                            DEFAULT_FLOAT_DIGITS_PRECISION);
 
-	    qjson_start_map(&context);
-	    qjson_add_string(&context, "null");
-	    qjson_add_null(&context);
-	    qjson_add_string(&context, "one");
-	    qjson_add_integer(&context, 1);
-	    qjson_add_string(&context, "list");
-	        qjson_start_list(&context);
-	        qjson_add_integer(&context, 1);
-	        qjson_add_integer(&context, 2);
-	        qjson_add_integer(&context, 3);
-	            qjson_start_map(&context);
-	            qjson_add_string(&context, "a");
-	            qjson_add_integer(&context, 1);
-	            qjson_add_string(&context, "b");
-	            qjson_add_integer(&context, 2);
-	            qjson_add_string(&context, "c");
-	            qjson_add_integer(&context, 3);
-	            qjson_end_container(&context);
-	        qjson_end_container(&context);
-	    qjson_add_string(&context, "true");
-	    qjson_add_boolean(&context, true);
-	    qjson_end_container(&context);
-	    qjson_end_encoding(&context);
+        qjson_start_map(&context);
+        qjson_add_string(&context, "null");
+        qjson_add_null(&context);
+        qjson_add_string(&context, "one");
+        qjson_add_integer(&context, 1);
+        qjson_add_string(&context, "list");
+            qjson_start_list(&context);
+            qjson_add_integer(&context, 1);
+            qjson_add_integer(&context, 2);
+            qjson_add_integer(&context, 3);
+                qjson_start_map(&context);
+                qjson_add_string(&context, "a");
+                qjson_add_integer(&context, 1);
+                qjson_add_string(&context, "b");
+                qjson_add_integer(&context, 2);
+                qjson_add_string(&context, "c");
+                qjson_add_integer(&context, 3);
+                qjson_end_container(&context);
+            qjson_end_container(&context);
+        qjson_add_string(&context, "true");
+        qjson_add_boolean(&context, true);
+        qjson_end_container(&context);
+        qjson_end_encoding(&context);
 
-	    printf("%s\n", buffer);
-	}
+        printf("%s\n", buffer);
+    }
 
 #### Result:
 
-	{
-	    "null": null,
-	    "one": 1,
-	    "list": [
-	        1,
-	        2,
-	        3,
-	        {
-	            "a": 1,
-	            "b": 2,
-	            "c": 3
-	        }
-	    ],
-	    "true": true
-	}
+    {
+        "null": null,
+        "one": 1,
+        "list": [
+            1,
+            2,
+            3,
+            {
+                "a": 1,
+                "b": 2,
+                "c": 3
+            }
+        ],
+        "true": true
+    }
 
 
 
 ### Parsing
 
-	#include <qjson/qjson.h>
-	#include <stdio.h>
+    #include <qjson/qjson.h>
+    #include <stdio.h>
 
-	typedef struct
-	{
-	    int depth;
-	} json_parse_context;
+    typedef struct
+    {
+        int depth;
+    } json_parse_context;
 
-	static void indent(json_parse_context* context)
-	{
-	    for(int i = 0; i < context->depth; i++)
-	    {
-	        printf("  ");
-	    }
-	}
+    static void indent(json_parse_context* context)
+    {
+        for(int i = 0; i < context->depth; i++)
+        {
+            printf("  ");
+        }
+    }
 
-	static void on_parse_error(void* context, const char* message)
-	{
-	    printf("Error: %s\n", message);
-	}
+    static void on_parse_error(void* context, const char* message)
+    {
+        printf("Error: %s\n", message);
+    }
 
-	static void on_string(void* context, const char* str)
-	{
-	    indent((json_parse_context*)context);
-	    printf("\"%s\"\n", str);
-	}
+    static void on_string(void* context, const char* str)
+    {
+        indent((json_parse_context*)context);
+        printf("\"%s\"\n", str);
+    }
 
-	static void on_null(void* context)
-	{
-	    indent((json_parse_context*)context);
-	    printf("null\n");
-	}
-	    
-	static void on_bool(void* context, bool value)
-	{
-	    indent((json_parse_context*)context);
-	    printf(value ? "true\n" : "false\n");
-	}
-	    
-	static void on_int(void* context, int64_t value)
-	{
-	    indent((json_parse_context*)context);
-	    printf("%ld\n", value);
-	}
-	    
-	static void on_float(void* context, double value)
-	{
-	    indent((json_parse_context*)context);
-	    printf("%lg\n", value);
-	}
+    static void on_null(void* context)
+    {
+        indent((json_parse_context*)context);
+        printf("null\n");
+    }
+        
+    static void on_bool(void* context, bool value)
+    {
+        indent((json_parse_context*)context);
+        printf(value ? "true\n" : "false\n");
+    }
+        
+    static void on_int(void* context, int64_t value)
+    {
+        indent((json_parse_context*)context);
+        printf("%ld\n", value);
+    }
+        
+    static void on_float(void* context, double value)
+    {
+        indent((json_parse_context*)context);
+        printf("%lg\n", value);
+    }
 
-	static void on_list_start(void* context)
-	{
-	    indent((json_parse_context*)context);
-	    printf("[\n");
-	    ((json_parse_context*)context)->depth++;
-	}
-	    
-	static void on_list_end(void* context)
-	{
-	    ((json_parse_context*)context)->depth--;
-	    indent((json_parse_context*)context);
-	    printf("]\n");
-	}
-	    
-	static void on_map_start(void* context)
-	{
-	    indent((json_parse_context*)context);
-	    printf("{\n");
-	    ((json_parse_context*)context)->depth++;
-	}
-	    
-	static void on_map_end(void* context)
-	{
-	    ((json_parse_context*)context)->depth--;
-	    indent((json_parse_context*)context);
-	    printf("}\n");
-	}
+    static void on_list_start(void* context)
+    {
+        indent((json_parse_context*)context);
+        printf("[\n");
+        ((json_parse_context*)context)->depth++;
+    }
+        
+    static void on_list_end(void* context)
+    {
+        ((json_parse_context*)context)->depth--;
+        indent((json_parse_context*)context);
+        printf("]\n");
+    }
+        
+    static void on_map_start(void* context)
+    {
+        indent((json_parse_context*)context);
+        printf("{\n");
+        ((json_parse_context*)context)->depth++;
+    }
+        
+    static void on_map_end(void* context)
+    {
+        ((json_parse_context*)context)->depth--;
+        indent((json_parse_context*)context);
+        printf("}\n");
+    }
 
-	void parse_example()
-	{
-	    qjson_parse_callbacks callbacks;
-	    callbacks.on_null = on_null;
-	    callbacks.on_boolean = on_bool;
-	    callbacks.on_int = on_int;
-	    callbacks.on_float = on_float;
-	    callbacks.on_string = on_string;
-	    callbacks.on_parse_error = on_parse_error;
-	    callbacks.on_list_start = on_list_start;
-	    callbacks.on_list_end = on_list_end;
-	    callbacks.on_map_start = on_map_start;
-	    callbacks.on_map_end = on_map_end;
+    void parse_example()
+    {
+        qjson_parse_callbacks callbacks;
+        callbacks.on_null = on_null;
+        callbacks.on_boolean = on_bool;
+        callbacks.on_int = on_int;
+        callbacks.on_float = on_float;
+        callbacks.on_string = on_string;
+        callbacks.on_parse_error = on_parse_error;
+        callbacks.on_list_start = on_list_start;
+        callbacks.on_list_end = on_list_end;
+        callbacks.on_map_start = on_map_start;
+        callbacks.on_map_end = on_map_end;
 
-	    json_parse_context context = {0};
-	    const char* json_string = "{\"null\":null,\"one\":1,\"list\":[1,2,3,{\"a\":1,\"b\":2,\"c\":3}],\"true\":true}";
-	    qjson_parse_string(json_string, &callbacks, &context);
-	}
+        json_parse_context context = {0};
+        const char* json_string = "{\"null\":null,\"one\":1,\"list\":[1,2,3,{\"a\":1,\"b\":2,\"c\":3}],\"true\":true}";
+        qjson_parse_string(json_string, &callbacks, &context);
+    }
 
 #### Result:
 
-	{
-	  "null"
-	  null
-	  "one"
-	  1
-	  "list"
-	  [
-	    1
-	    2
-	    3
-	    {
-	      "a"
-	      1
-	      "b"
-	      2
-	      "c"
-	      3
-	    }
-	  ]
-	  "true"
-	  true
-	}
+    {
+      "null"
+      null
+      "one"
+      1
+      "list"
+      [
+        1
+        2
+        3
+        {
+          "a"
+          1
+          "b"
+          2
+          "c"
+          3
+        }
+      ]
+      "true"
+      true
+    }
 
 
 
--- src/library.c
+++ src/library.c
@@ -7,7 +7,7 @@
 
 static inline bool has_room_for_bytes(qjson_encode_context* context, int byte_count)
 {
-	return context->pos + byte_count <= context->end;
+    return context->pos + byte_count <= context->end;
 }
 
 const char* qjson_version()
@@ -38,218 +38,218 @@ qjson_encode_context qjson_new_encode_context_with_config(uint8_t* const memory_
 
 qjson_encode_context qjson_new_encode_context(uint8_t* const memory_start, uint8_t* const memory_end)
 {
-	return qjson_new_encode_context_with_config(memory_start,
-												memory_end,
-												DEFAULT_INDENT_SPACES,
-												DEFAULT_FLOAT_DIGITS_PRECISION);
+    return qjson_new_encode_context_with_config(memory_start,
+                                                memory_end,
+                                                DEFAULT_INDENT_SPACES,
+                                                DEFAULT_FLOAT_DIGITS_PRECISION);
 }
 
 static void add_bytes(qjson_encode_context* const context, const char* bytes, size_t length)
 {
-	memcpy(context->pos, bytes, length);
-	context->pos += length;
+    memcpy(context->pos, bytes, length);
+    context->pos += length;
 }
 
 static bool add_indentation(qjson_encode_context* const context)
 {
-	if(context->indent_spaces <= 0)
-	{
-		return true;
-	}
+    if(context->indent_spaces <= 0)
+    {
+        return true;
+    }
 
-	int num_spaces = context->indent_spaces * context->container_level;
-	if(!has_room_for_bytes(context, num_spaces + 1)) return false;
-	*context->pos++ = '\n';
-	memset(context->pos, ' ', num_spaces);
-	context->pos += num_spaces;
-	return true;
+    int num_spaces = context->indent_spaces * context->container_level;
+    if(!has_room_for_bytes(context, num_spaces + 1)) return false;
+    *context->pos++ = '\n';
+    memset(context->pos, ' ', num_spaces);
+    context->pos += num_spaces;
+    return true;
 }
 
 static bool begin_new_object(qjson_encode_context* const context)
 {
-	if(context->is_first_in_document)
-	{
-		context->is_first_in_document = false;
-		return true;
-	}
+    if(context->is_first_in_document)
+    {
+        context->is_first_in_document = false;
+        return true;
+    }
 
-	bool is_in_map = context->is_inside_map[context->container_level];
-	bool next_object_is_map_key = context->next_object_is_map_key;
+    bool is_in_map = context->is_inside_map[context->container_level];
+    bool next_object_is_map_key = context->next_object_is_map_key;
 
-	if(is_in_map)
-	{
-		context->next_object_is_map_key = !next_object_is_map_key;
-	}
+    if(is_in_map)
+    {
+        context->next_object_is_map_key = !next_object_is_map_key;
+    }
 
-	if(context->is_first_in_container)
-	{
-		if(!add_indentation(context)) return false;
-		context->is_first_in_container = false;
-		return true;
-	}
+    if(context->is_first_in_container)
+    {
+        if(!add_indentation(context)) return false;
+        context->is_first_in_container = false;
+        return true;
+    }
 
-	if(!has_room_for_bytes(context, 1)) return false;
+    if(!has_room_for_bytes(context, 1)) return false;
 
-	if(!is_in_map || next_object_is_map_key)
-	{
-		*context->pos++ = ',';
-		if(!add_indentation(context)) return false;
-		return true;
-	}
+    if(!is_in_map || next_object_is_map_key)
+    {
+        *context->pos++ = ',';
+        if(!add_indentation(context)) return false;
+        return true;
+    }
 
-	*context->pos++ = ':';
-	if(context->indent_spaces > 0)
-	{
-		if(!has_room_for_bytes(context, 1)) return false;
-		*context->pos++ = ' ';
-	}
-	return true;
+    *context->pos++ = ':';
+    if(context->indent_spaces > 0)
+    {
+        if(!has_room_for_bytes(context, 1)) return false;
+        *context->pos++ = ' ';
+    }
+    return true;
 }
 
 static bool add_object(qjson_encode_context* const context, const char* encoded_object)
 {
-	if(!begin_new_object(context)) return false;
-	int length = strlen(encoded_object);
-	if(!has_room_for_bytes(context, length)) return false;
-	add_bytes(context, encoded_object, length);
-	return true;
+    if(!begin_new_object(context)) return false;
+    int length = strlen(encoded_object);
+    if(!has_room_for_bytes(context, length)) return false;
+    add_bytes(context, encoded_object, length);
+    return true;
 }
 
 bool qjson_add_null(qjson_encode_context* const context)
 {
-	if(context->next_object_is_map_key) return false;
-	return add_object(context, "null");
+    if(context->next_object_is_map_key) return false;
+    return add_object(context, "null");
 }
 
 bool qjson_add_boolean(qjson_encode_context* const context, const bool value)
 {
-	if(context->next_object_is_map_key) return false;
-	return add_object(context, value ? "true" : "false");
+    if(context->next_object_is_map_key) return false;
+    return add_object(context, value ? "true" : "false");
 }
 
 bool qjson_add_integer(qjson_encode_context* const context, const int64_t value)
 {
-	if(context->next_object_is_map_key) return false;
-	char buffer[21];
-	sprintf(buffer, "%ld", value);
-	return add_object(context, buffer);
+    if(context->next_object_is_map_key) return false;
+    char buffer[21];
+    sprintf(buffer, "%ld", value);
+    return add_object(context, buffer);
 }
 
 bool qjson_add_float(qjson_encode_context* const context, const double value)
 {
-	if(context->next_object_is_map_key) return false;
-	char fmt[10];
-	sprintf(fmt, "%%.%dlg", context->float_digits_precision);
-	char buffer[context->float_digits_precision + 2];
-	sprintf(buffer, fmt, value);
-	return add_object(context, buffer);
+    if(context->next_object_is_map_key) return false;
+    char fmt[10];
+    sprintf(fmt, "%%.%dlg", context->float_digits_precision);
+    char buffer[context->float_digits_precision + 2];
+    sprintf(buffer, fmt, value);
+    return add_object(context, buffer);
 }
 
 static char get_escape_char(char ch)
 {
-	switch(ch)
-	{
-		case '\\': return '\\';
-		case  '"': return '"';
-		case '\b': return 'b';
-		case '\f': return 'f';
-		case '\n': return 'n';
-		case '\r': return 'r';
-		case '\t': return 't';
-		default:   return 0;
-	}
+    switch(ch)
+    {
+        case '\\': return '\\';
+        case  '"': return '"';
+        case '\b': return 'b';
+        case '\f': return 'f';
+        case '\n': return 'n';
+        case '\r': return 'r';
+        case '\t': return 't';
+        default:   return 0;
+    }
 }
 
 static bool add_substring_with_escaping(qjson_encode_context* const context, const char* const start, const char* const end)
 {
-	for(const char* src = start; src < end; src++)
-	{
-		char ch = *src;
-		char escape_ch = get_escape_char(ch);
-		if(escape_ch != 0)
-		{
-			if(!has_room_for_bytes(context, 2)) return false;
-			*context->pos++ = '\\';
-			*context->pos++ = escape_ch;
-		}
-		else
-		{
-			if(!has_room_for_bytes(context, 1)) return false;
-			*context->pos++ = ch;
-		}
-	}
-	return true;
+    for(const char* src = start; src < end; src++)
+    {
+        char ch = *src;
+        char escape_ch = get_escape_char(ch);
+        if(escape_ch != 0)
+        {
+            if(!has_room_for_bytes(context, 2)) return false;
+            *context->pos++ = '\\';
+            *context->pos++ = escape_ch;
+        }
+        else
+        {
+            if(!has_room_for_bytes(context, 1)) return false;
+            *context->pos++ = ch;
+        }
+    }
+    return true;
 }
 
 bool qjson_add_substring(qjson_encode_context* const context, const char* const start, const char* const end)
 {
-	size_t byte_count = end - start;
-	if(!add_object(context, "\"")) return false;
-	if(!has_room_for_bytes(context, byte_count + 1)) return false;
-	add_substring_with_escaping(context, start, end);
-	add_bytes(context, "\"", 1);
-	return true;
+    size_t byte_count = end - start;
+    if(!add_object(context, "\"")) return false;
+    if(!has_room_for_bytes(context, byte_count + 1)) return false;
+    add_substring_with_escaping(context, start, end);
+    add_bytes(context, "\"", 1);
+    return true;
 }
 
 bool qjson_add_string(qjson_encode_context* const context, const char* const str)
 {
-	return qjson_add_substring(context, str, str + strlen(str));
+    return qjson_add_substring(context, str, str + strlen(str));
 }
 
 static bool start_container(qjson_encode_context* const context, bool is_map)
 {
-	if(context->next_object_is_map_key) return false;
-	begin_new_object(context);
-	if(!has_room_for_bytes(context, 1)) return false;
-	add_bytes(context, is_map ? "{" : "[", 1);
-	context->container_level++;
-	context->is_first_in_container = true;
-	context->is_inside_map[context->container_level] = is_map;
-	context->next_object_is_map_key = is_map;
-	return true;
+    if(context->next_object_is_map_key) return false;
+    begin_new_object(context);
+    if(!has_room_for_bytes(context, 1)) return false;
+    add_bytes(context, is_map ? "{" : "[", 1);
+    context->container_level++;
+    context->is_first_in_container = true;
+    context->is_inside_map[context->container_level] = is_map;
+    context->next_object_is_map_key = is_map;
+    return true;
 
 }
 
 bool qjson_start_list(qjson_encode_context* const context)
 {
-	return start_container(context, false);
+    return start_container(context, false);
 }
 
 bool qjson_start_map(qjson_encode_context* const context)
 {
-	return start_container(context, true);
+    return start_container(context, true);
 }
 
 bool qjson_end_container(qjson_encode_context* const context)
 {
-	if(context->container_level <= 0)
-	{
-		return false;
-	}
-	bool is_in_map = context->is_inside_map[context->container_level];
-	if(is_in_map && !context->next_object_is_map_key)
-	{
-		return false;
-	}
+    if(context->container_level <= 0)
+    {
+        return false;
+    }
+    bool is_in_map = context->is_inside_map[context->container_level];
+    if(is_in_map && !context->next_object_is_map_key)
+    {
+        return false;
+    }
 
-	context->container_level--;
-	if(!add_indentation(context)) return false;
-	if(!has_room_for_bytes(context, 1)) return false;
-	add_bytes(context, is_in_map ? "}" : "]", 1);
-	context->next_object_is_map_key = context->is_inside_map[context->container_level];
-	return true;
+    context->container_level--;
+    if(!add_indentation(context)) return false;
+    if(!has_room_for_bytes(context, 1)) return false;
+    add_bytes(context, is_in_map ? "}" : "]", 1);
+    context->next_object_is_map_key = context->is_inside_map[context->container_level];
+    return true;
 }
 
 const char* qjson_end_encoding(qjson_encode_context* const context)
 {
-	while(context->container_level > 0)
-	{
-		if(!qjson_end_container(context))
-		{
-			return NULL;
-		}
-	}
-	if(!has_room_for_bytes(context, 1)) return NULL;
-	*context->pos = 0;
-	return (const char*)context->pos;
+    while(context->container_level > 0)
+    {
+        if(!qjson_end_container(context))
+        {
+            return NULL;
+        }
+    }
+    if(!has_room_for_bytes(context, 1)) return NULL;
+    *context->pos = 0;
+    return (const char*)context->pos;
 }
